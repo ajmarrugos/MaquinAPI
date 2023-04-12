26a182a5a05b671bb677d2b7227bfe60f2c35c23
ajmarrugos <ajmarrugos@gmail.com>
2023-04-12 16:04:13 -0500

    Starting Object Modeling

---
+++ MaquinAPI/Models/Object.cs
@@ -0,0 +1,39 @@
+namespace MaquinAPI.Models;
+
+// Default Object Model Template
+public class Object
+{
+    // Public Definitions
+    public Guid Id { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public DateTime StartDateTime { get; }
+    public DateTime EndDateTime { get; }
+    public DateTime LastModifiedDateTime { get; }
+    public List<string> Tags { get; }
+    public List<string> Labels { get; }
+
+    // Object Constructor
+    public Object(
+        Guid id,
+        string name,
+        string description,
+        DateTime startDateTime,
+        DateTime endDateTime
+        DateTime lastModifiedDateTime,
+        List<string> tags,
+        List<string> labels)
+    {
+        // Enforce Invariants
+        Id = id;
+        Name = name;
+        Description = description;
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime ;
+        LastModifiedDateTime = lastModifiedDateTime;
+        Tags = tags; 
+        Labels = labels;
+    }
+}
+
+
