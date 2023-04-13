a7a7b1fa6dd07aa630967942c29296d1c68d9ec2
Alberto Marrugo Saldarriaga <82329337+ajmarrugos@users.noreply.github.com>
2023-04-13 15:37:38 -0500

    Adaptable API Solution (#9)
    * Single API Architecture Solution
    
    * Remove external library
    
    * Implementing Map Controllers & SwaggerUI
    
    * .gitignore Updated
    
    * New user class
    
    * New Users Controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using MaquinAPI;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace MaquinAPI.Controllers
+{
+    // API CONTROLLER
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // Simulated User template
+        private static List<User> users = new List<User>
+        {
+            new User
+            {
+                Id = 1,
+                Name = "Alberto Marrugo",
+                Username = "ajmarrugos",
+                Email = "ajmarrugos@gmail.com",
+                Address = new Address
+                {
+                    Street = "Boyaca",
+                    Suite = "Apt. 007",
+                    City = "Bogota",
+                    Zipcode = "0101010",
+                    Geo = new Geo
+                    {
+                        Lat = "-37.3159",
+                        Lng = "81.1496"
+                    }
+                },
+                Phone = "57-300-416-1450",
+                Website = "github.com/ajmarrugos",
+                Company = new Company
+                {
+                    Name = "Unosquare",
+                    CatchPhrase = "Nearshore Software Engineers For Your Digital Transformation",
+                    Bs = "Digital Engineering"
+                }
+            }
+        };
+
+        // GET: api/Users/
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return Ok(users);
+        }
+
+        // POST api/Users/
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser()
+        {
+            return Ok(users);
+        }
+
+
+        // PUT api/Users/
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/Users/
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
