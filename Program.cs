43529736545bd7cbdfa67a28ced97d8d999230c0
ajmarrugos <ajmarrugos@gmail.com>
2023-04-13 19:08:21 -0500

    Integrated new Services & Tools to the dependency injection

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
+using MaquinAPI.Services;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<ExternalApiService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+    });
 }
 
 app.UseHttpsRedirection();
