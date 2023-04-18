63bb03413abe4b36a4ef2dcf2c1595779532dffa
ajmarrugos <ajmarrugos@gmail.com>
2023-04-18 11:14:46 -0500

    Added "Required" statement to User class definitions

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MaquinAPI.Models
 {
     public class User
     {
+        [Required]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
